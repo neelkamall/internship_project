0d7f27ad2c73953ddf41202a2215b1709ed81f2c
unknown <neelkamall>
2025-06-02 15:26:16 +0530

    new change

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,18 @@
-import './ExploreContainer.css';
+import './ExploreContainer.css'
 
-interface ContainerProps { }
 
-const ExploreContainer: React.FC<ContainerProps> = () => {
-  return (
-    <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+const ExploreContainer: React.FC = () => {
+  return (<><img src="\public\assets\images\aaa.jpg" alt="" /><div className='Neel'>This is level <span>Coding</span> one heading</div>
+
+  <div className='Rahul'>This is game<span>Best</span>Ever one</div>
+  <div className='Ajay'> This is<span> the best</span> Program</div>
+  <button className='clickhere'>click here</button>
+  <hr/>
+  <div className='Parent'>
+  <div className='child1'></div>
+  <div className= 'child2'></div>
+  </div>
+   </> 
   );
 };
 
